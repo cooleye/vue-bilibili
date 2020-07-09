a394dff3e6ea96e3fedc2ac6bcc9996a36b8aa19
davie kong <iscooleye@163.com>
2020-07-09 08:56:59 +0800

    add

--- js/Home.js
+++ js/Home.js
@@ -6,6 +6,7 @@ var Home = Vue.component("home",{
     data(){
         return{
             videos:[],
+            // showAllMenu:false,
             menuList:[
                 {id:0,title:"首页",url:'/home/0'},
                 {id:1,title:"动画",url:'/home/1'},
@@ -13,9 +14,32 @@ var Home = Vue.component("home",{
                 {id:3,title:"国创",url:'/home/3'},
                 {id:4,title:"音乐",url:'/home/4'},
             ],
+            allMenu:[
+                {id:0,title:"首页",url:'/home/0'},
+                {id:1,title:"动画",url:'/home/1'},
+                {id:2,title:"番剧",url:'/home/2'},
+                {id:3,title:"国创",url:'/home/3'},
+                {id:4,title:"音乐",url:'/home/4'},
+                {id:5,title:"首页",url:'/home/5'},
+                {id:6,title:"动画",url:'/home/6'},
+                {id:7,title:"番剧",url:'/home/7'},
+                {id:8,title:"国创",url:'/home/8'},
+                {id:9,title:"音乐",url:'/home/9'},
+                {id:10,title:"首页",url:'/home/10'},
+                {id:11,title:"动画",url:'/home/11'},
+                {id:12,title:"番剧",url:'/home/12'},
+                {id:13,title:"国创",url:'/home/13'},
+                {id:14,title:"音乐",url:'/home/14'},
+            ],
             swiperList:[
                 './static/b1.jpg','./static/b2.jpg','./static/b3.jpg',
             ]
+        }
+    },
+    watch: {
+        $route(){
+            let tab = this.$route.params.tab;
+            console.log(tab)
         }
     },
     mounted() {
@@ -33,6 +57,12 @@ var Home = Vue.component("home",{
             $.get('https://www.fastmock.site/mock/e5235ba89a269aad3a0c14cd975023bf/api/home',res => {
                 this.videos = res;
             })
+        },
+        moreMenu(){
+            $('.all-menu').slideDown()
+        },
+        menuSlideUp(){
+            $('.all-menu').slideUp()
         }
     },
 })
